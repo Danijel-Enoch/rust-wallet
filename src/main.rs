d540a162bcd3d2513e5b775236a41fae1f6766c3
Danijel-Enoch <enochdanijel@gmail.com>
2022-08-03 17:44:05 +0100

    git init

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<()> {
     let keypair = wallets::create_keypair()?;
     println!("{:?}", keypair);
     let web3 = wallets::establish_connection(URL)?;
-    let to_address = Address::from_str("0x1b42ee3294DA8A1Fa125061674067128e2AFEB6c")?;
+    let to_address = Address::from_str("0x1b42ee3294DA8A1Fa125061674067128e2AB6c")?;
     let tx_object = wallets::create_tx_object(to_address,7)?;
     let result = wallets::sign_tx(web3.clone(),tx_object,keypair.0).await?;
     println!("result {:?}", result);
